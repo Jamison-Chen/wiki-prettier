41357e6e6570e541611a495aad1ac59479d91964
Jamison Chen <jamisonforeverandever@gmail.com>
2021-02-27 03:14:58 +0800

    Successfully connected to Api.

--- frontEnd/modules/script.js
+++ frontEnd/modules/script.js
@@ -1,14 +1,16 @@
 "use strict";
 const testBtn = document.getElementById("test-btn");
+const body = document.getElementById("body");
 if (testBtn != null) {
     testBtn.addEventListener("click", function () {
-        fetch('https://wiki-scraper.herokuapp.com/')
+        const wikiUrl = "https://en.wikipedia.org/wiki/NP-completeness";
+        fetch(`https://wiki-scraper.herokuapp.com/fetchContent?url=${wikiUrl}`)
             .then(function (response) {
-            // console.log(response.json());
+            // console.log(response);
             return response.json();
         })
             .then(function (myJson) {
-            console.log(myJson);
+            body === null || body === void 0 ? void 0 : body.innerHTML = myJson["body"];
         });
     });
 }
--- frontEnd/script.ts
+++ frontEnd/script.ts
@@ -1,13 +1,15 @@
 const testBtn: HTMLElement | null = document.getElementById("test-btn");
+const body: HTMLElement | null = document.getElementById("body");
 if (testBtn != null) {
     testBtn.addEventListener("click", function () {
-        fetch('https://wiki-scraper.herokuapp.com/')
+        const wikiUrl: string = "https://en.wikipedia.org/wiki/NP-completeness";
+        fetch(`https://wiki-scraper.herokuapp.com/fetchContent?url=${wikiUrl}`)
             .then(function (response) {
-                // console.log(response.json());
+                // console.log(response);
                 return response.json();
             })
             .then(function (myJson) {
-                console.log(myJson);
+                body?.innerHTML = myJson["body"];
             });
     });
 }
