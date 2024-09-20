b2b02b3fa5e58761119dd5f64c49ad28eec48a70
Bryan Chen <bryan.chen.429@gmail.com>
2024-09-20 13:17:40 +0800

    See description: 1. Add some meta tags for SEO at homepage. 2. Fix display issue of homepage.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,20 @@
 "use strict";
-const searchBtn = document.getElementById("search-btn");
-const inputUrl = document.getElementById("search-bar");
+const domSearchInput = document.getElementById("search-input");
 const hint = document.getElementById("hint");
-window.addEventListener("keyup", search);
-if (searchBtn != null) {
-    searchBtn.addEventListener("click", search);
-}
-function search(e) {
-    if (e instanceof KeyboardEvent && e.key !== "Enter") {
-        if (document.activeElement == inputUrl) {
-            checkInputUrl();
-        }
+window.addEventListener("keyup", (e) => {
+    if (e.key === "Enter" && domSearchInput.value && checkInputUrl()) {
+        window.open(`./view/?url=${domSearchInput.value}`, "_blank");
+        domSearchInput.value = "";
     }
-    else if (checkInputUrl() &&
-        inputUrl instanceof HTMLInputElement &&
-        inputUrl.value) {
-        window.open(`./view/?url=${inputUrl.value}`, "_blank");
-        inputUrl.value = "";
-    }
-}
-if (inputUrl != null) {
-    inputUrl.addEventListener("focus", checkInputUrl);
-}
+    else
+        checkInputUrl();
+});
+domSearchInput.addEventListener("focus", checkInputUrl);
 function checkInputUrl() {
-    if (inputUrl instanceof HTMLInputElement && hint != null) {
-        if (inputUrl.value.indexOf("wikipedia.org") == -1) {
-            hint.style.opacity = "100%";
-            return false;
-        }
-        else {
-            hint.style.opacity = "0%";
-        }
+    if (domSearchInput.value.indexOf("wikipedia.org") == -1) {
+        hint.style.opacity = "100%";
+        return false;
     }
+    hint.style.opacity = "0%";
     return true;
 }
--- main.ts
+++ main.ts
@@ -1,40 +1,21 @@
-const searchBtn: HTMLElement | null = document.getElementById("search-btn");
-const inputUrl: HTMLElement | null = document.getElementById("search-bar");
-const hint: HTMLElement | null = document.getElementById("hint");
+const domSearchInput: HTMLInputElement = document.getElementById(
+    "search-input"
+) as HTMLInputElement;
+const hint: HTMLElement = document.getElementById("hint")!;
 
-window.addEventListener("keyup", search);
+window.addEventListener("keyup", (e: KeyboardEvent) => {
+    if (e.key === "Enter" && domSearchInput.value && checkInputUrl()) {
+        window.open(`./view/?url=${domSearchInput.value}`, "_blank");
+        domSearchInput.value = "";
+    } else checkInputUrl();
+});
 
-if (searchBtn != null) {
-    searchBtn.addEventListener("click", search);
-}
-
-function search(e: Event): void {
-    if (e instanceof KeyboardEvent && e.key !== "Enter") {
-        if (document.activeElement == inputUrl) {
-            checkInputUrl();
-        }
-    } else if (
-        checkInputUrl() &&
-        inputUrl instanceof HTMLInputElement &&
-        inputUrl.value
-    ) {
-        window.open(`./view/?url=${inputUrl.value}`, "_blank");
-        inputUrl.value = "";
-    }
-}
-
-if (inputUrl != null) {
-    inputUrl.addEventListener("focus", checkInputUrl);
-}
-
+domSearchInput.addEventListener("focus", checkInputUrl);
 function checkInputUrl(): boolean {
-    if (inputUrl instanceof HTMLInputElement && hint != null) {
-        if (inputUrl.value.indexOf("wikipedia.org") == -1) {
-            hint.style.opacity = "100%";
-            return false;
-        } else {
-            hint.style.opacity = "0%";
-        }
+    if (domSearchInput.value.indexOf("wikipedia.org") == -1) {
+        hint.style.opacity = "100%";
+        return false;
     }
+    hint.style.opacity = "0%";
     return true;
 }
